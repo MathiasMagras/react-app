49a9435faef5bd1f33ed05e11aa2310d22534a39
DESKTOP-544KVUK\ALMA <Mathias@onyxtech.ca>
2022-07-04 14:31:36 -0400

    semantic release

---
+++ src/release.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/MathiasMagras/react-app.git",
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/github"]
+}
